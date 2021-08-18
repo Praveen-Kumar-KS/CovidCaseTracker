618b54a4de3ed8ad4718d7bcc583c3ea4a0eed05
Praveen-Kumar-KS <66106461+Praveen-Kumar-KS@users.noreply.github.com>
2021-08-18 18:41:10 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+/*if(country.length==0||startdate.length==0||enddate.length==0)
+    {
+      alert("Invalid");
+    }*/
+//var url=`https://api.covid19api.com/country/${country}?from=${startdate}T00:00:00Z&to=${enddate}T00:00:00Z`;
+//var xhr= new XMLHttpRequest();
